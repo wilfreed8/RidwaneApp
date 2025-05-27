b7a3c34f8ac9f6d9ab6bf208fd60d1a3b67e7d63
Hackman <wilfreednouame@gmail.com>
2025-05-27 04:52:07 +0200

    5 commit

--- API/index.php
+++ API/index.php
@@ -1,13 +1,15 @@
 <?php
 
-require __DIR__ . '/../public/index.php';  
+require __DIR__ . '/../laravel/vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../laravel/bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-$response = tap($kernel->handle($request))->send();
+$response->send();
 
 $kernel->terminate($request, $response);
